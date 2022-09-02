facae5375706ba258920f29b7f5982c2e1ebb01d
RamonOliveira95 <ramonsilvadeoliveira95@hotmail.com>
2022-09-01 22:40:54 -0300

    0.4

--- lib/personagens/personagens_filmes.dart
+++ lib/personagens/personagens_filmes.dart
@@ -1,3 +1,63 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class PersonagensFilmes extends StatefulWidget {
+  const PersonagensFilmes({Key? key}) : super(key: key);
+
+  @override
+  State<PersonagensFilmes> createState() => _PersonagensFilmesState();
+}
+
+class _PersonagensFilmesState extends State<PersonagensFilmes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Personagens"),
+      ),
+      body: FutureBuilder<dynamic>(
+        future: pegarPersonagem(),
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index){
+                var personagem = snapshot.data![index];
+              return ListTile(
+                leading: CircleAvatar(
+                  child: Text('1'),
+                ),
+                title: Text(personagem["name"]),
+              );
+            });
+          } else if (snapshot.hasError) {
+            return Text("${snapshot.error}");
+          }
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+    );
+  }
+
+  pegarPersonagem() async {
+    var url = Uri.parse('https://swapi.dev/api/people/');
+    var resposta = await http.get(url);
+    if (resposta.statusCode == 200) {
+      return jsonDecode(resposta.body);
+    } else {
+      throw Exception("Não foi possível carregar os personagens");
+    }
+  }
+}
+
+
+
+//TENTATIVA 0.3
+/*
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'package:flutter/material.dart';
@@ -64,3 +124,4 @@ class _PersonagensState extends State<PersonagensFilmes> {
     }
   }
 }
+*/
